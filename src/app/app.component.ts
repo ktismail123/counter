dfef6b2ddfb48c3b0f2a6e99f5229d82f26835fd
_kt_ismail <_kt_ismail@kt-ismails-Air.bbrouter>
2024-07-20 00:36:59 +0530

    sound added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,10 @@ export class AppComponent implements OnInit, OnDestroy{
   delay: number = 1; // Default delay in seconds
   intervalId: any;
 
+  sound = false;
+
+  incrementSound = new Audio('assets/sound2.mp3');
+
   ngOnInit() {
     this.startAutoIncrement();
   }
@@ -26,6 +30,8 @@ export class AppComponent implements OnInit, OnDestroy{
 
   increment() {
     this.counter += 1;
+    if(this.sound)
+    this.playSound(this.incrementSound);
   }
 
   decrement() {
@@ -34,6 +40,7 @@ export class AppComponent implements OnInit, OnDestroy{
 
   reset() {
     this.counter = 0;
+    
   }
 
   togglePause() {
@@ -67,4 +74,19 @@ export class AppComponent implements OnInit, OnDestroy{
       this.intervalId = null;
     }
   }
+
+  soundEvent(): void {
+    this.sound = !this.sound;
+  }
+
+  playSound(sound: HTMLAudioElement) {
+    sound.currentTime = 0; // Restart the audio from the beginning
+    sound.play(); // Start playing the sound
+  
+    // Pause the sound after a short delay
+    setTimeout(() => {
+      sound.pause(); // Pause the sound
+    }, 800 ); // Delay duration in milliseconds
+  }
+  
 }
