814575b5c9b7f2d313e716a0c3f705faf8b166d6
_kt_ismail <_kt_ismail@kt-ismails-MacBook-Air.local>
2024-07-19 21:25:55 +0530

    project added to git hub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'counter';
+export class AppComponent implements OnInit, OnDestroy{
+  ttitle = 'counter';
+  counter: number = 0;
+  isPaused: boolean = true;
+  delay: number = 3; // Default delay in seconds
+  intervalId: any;
+
+  ngOnInit() {
+    this.startAutoIncrement();
+  }
+
+  ngOnDestroy() {
+    this.clearInterval();
+  }
+
+  increment() {
+    this.counter += 1;
+  }
+
+  decrement() {
+    this.counter -= 1;
+  }
+
+  reset() {
+    this.counter = 0;
+  }
+
+  togglePause() {
+    this.isPaused = !this.isPaused;
+    if (this.isPaused) {
+      this.clearInterval();
+    } else {
+      this.startAutoIncrement();
+    }
+  }
+
+  setDelay() {
+    if (!this.isPaused) {
+      this.clearInterval();
+      this.startAutoIncrement();
+    }
+  }
+
+  startAutoIncrement() {
+    this.clearInterval();
+    this.intervalId = setInterval(() => {
+      if (!this.isPaused) {
+        this.increment();
+      }
+    }, this.delay * 1000);
+  }
+
+  clearInterval() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  }
 }
